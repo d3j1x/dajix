783d53449b34e73bda85464a8423f96098f191e4
d3j1x <d3j1x@yahoo.com>
2023-07-19 13:50:06 +0200

    onMount

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -25,11 +25,11 @@ function toggleDarkMode() {
 
     // Update CSS variables based on the mode
     if (isDarkMode) {
-      document.documentElement.classList.remove('light');
       document.documentElement.classList.add('dark');
+      document.documentElement.classList.remove('light');
     } else {
-      document.documentElement.classList.remove('dark');
       document.documentElement.classList.add('light');
+      document.documentElement.classList.remove('dark');
     }
   }
 
